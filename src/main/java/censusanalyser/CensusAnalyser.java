b3747a2b035bbf14cda49d05e6c48a02caf19132
julinbestina <julin.bestina@yahoo.com>
2022-06-28 00:11:40 +0530

    Added Welcome message in master branch

---
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,16 @@
+package censusanalyser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class CensusAnalyser {
+  public static void main(String[] args) {
+      System.out.println("Welcome to Census Analyser Program");
+  }
+}
---
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,10 @@
+package censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CensusAnalyserTest {
+
+
+}
